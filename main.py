55d14c70487b8d0a6ed42bde2c664bbb5feab8db
TonyGael <anthonygaelmayol@gmail.com>
2024-03-22 22:37:10 -0300

    adding methods explanations on class Person

--- main.py
+++ main.py
@@ -5,15 +5,23 @@
 #         self.age = age
 #
 #     def __del__(self):
-#         print("Obejct is being deconstructured!")
+#         print("Object is being deconstructed!")
 #
-# p = Person("Chris", 25)
+#
+# p = Person("Tony", 33)
+
 class Vector:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    '''
+        def __add__(self, other):
+        Este método especial permite sumar dos vectores. Toma otro objeto de la clase Vector como argumento (other).
+        Devuelve un nuevo objeto Vector cuyas componentes x y y son la suma de las componentes correspondientes del
+        objeto actual (self) y el objeto other.
+    '''
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
@@ -27,6 +35,7 @@ class Vector:
 v1 = Vector(10, 20)
 v2 = Vector(50, 60)
 v3 = v1 + v2
+print(v3) # unless we change the representation method the result obtainded is: <__main__>.Vector object at 0X... something
 
 print(v3.x)
 print(v3.y)
